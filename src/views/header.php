1e763d1640f5018426144bce62910b53d7b5a8fc
Juan José Ruiz Cruz <rcjuanjo3156@gmail.com>
2023-04-03 12:44:59 -0600

    adding the navbar

--- src/views/header.php
+++ src/views/header.php
@@ -4,19 +4,22 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700;900&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="<?php echo RUTA;?>/dist/css/app.css">
     <title>Pokemon App</title>
 </head>
 <body>
     <header class="header">
-        <div class="icono">
-            <h1>POKEMON PHP APP</h1>
+        <div class="header__icon">
+            <h1>Pokemon php App</h1>
         </div>
-        <div class="menu">
-            <nav class="navegation">
-                <ul>
-                    <li><a href="#">Log in</a></li>
-                    <li><a href="#">Sing Up</a></li>
+        <div class="header__bar">
+            <nav class="navigation">
+                <ul class="navigation__ul">
+                    <li class="navigation__li"><a href="#">Log in</a></li>
+                    <li class="navigation__li"><a href="#">Sing Up</a></li>
                 </ul>
             </nav>
         </div>
