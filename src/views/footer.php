2d259270a4593091e294470a3c5dd69260093593
Juan José Ruiz Cruz <rcjuanjo3156@gmail.com>
2023-04-03 03:32:44 -0600

    adding the header and the footer to the view of the index.php and adding the view to the index.php

---
+++ src/views/footer.php
@@ -0,0 +1,5 @@
+<footer class="footer">
+    <p>Made by Juan José Ruiz Cruz</p>
+</footer>
+</body>
+</html>
---
+++ src/views/index.view.php
@@ -0,0 +1,4 @@
+<?php require('header.php'); ?>
+
+    <h1>Hola</h1>
+<?php require('footer.php'); ?>
