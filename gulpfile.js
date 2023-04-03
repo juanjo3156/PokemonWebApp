da48ab07ac0eeda79e6eedcbb1bb2047cc998ce4
Juan José Ruiz Cruz <rcjuanjo3156@gmail.com>
2023-04-03 03:27:14 -0600

    Adding css function

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,11 @@
 const {src,watch,dest} = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 
-function css(){
+function css(done){
     src('src/styles/app.scss')
         .pipe(sass())
         .pipe(dest('dist/css'));
+    done();
 }
 
 exports.css = css;
