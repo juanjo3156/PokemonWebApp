36c889ef138730b4008af4ae826d3d4b35c3045c
Juan José Ruiz Cruz <rcjuanjo3156@gmail.com>
2023-04-05 10:45:55 -0600

    Adding 'get_posts and 'page_number' to functions.php

--- src/functions.php
+++ src/functions.php
@@ -7,3 +7,17 @@
     function actual_page(){
         return isset($_GET['p']) ? (int)$_GET['p'] : 1 ; 
     }
+    function get_posts($post_by_page,$connection){
+        $start = (actual_page()>1)? actual_page()*$post_by_page - $post_by_page: 0;
+        $statement = $connection->prepare("SELECT SQL_CALC_FOUND_ROWS * FROM pokemon_list LIMIT $start,$post_by_page");
+        $statement->execute();
+        return $statement->fetchAll();
+    }
+    function page_number($post_by_page,$connection){
+        $total_posts = $connection->prepare('SELECT FOUND_ROWS() as total');
+        $total_posts->execute();
+        $total_posts = $total_posts->fetch()['total'];
+
+        $page_number = ceil($total_posts/$post_by_page);
+        return $page_number;
+    }
