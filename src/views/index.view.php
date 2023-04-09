0dca5bab410edecf945d71c187ca82be115e86be
Juan José Ruiz Cruz <rcjuanjo3156@gmail.com>
2023-04-09 17:28:01 -0600

    fixing form bug

--- src/views/index.view.php
+++ src/views/index.view.php
@@ -22,7 +22,7 @@
           <td><?php echo $pokemon['type']?></td>
           <td><?php echo $pokemon['region']?></td>
           <td class="action_column">
-            <form action="<? echo htmlspecialchars($_SERVER['PHP_SELF'])?>" method="post">
+            <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']);?>" method="post">
               <input type="hidden" name="pokemon_id" value="<?php echo $pokemon['id']?>">
               <button class = "btn-submit "type="submit">Add to favorites  <i class="fa-solid fa-heart"></i></button>
             </form>
