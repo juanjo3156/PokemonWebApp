5991bb4135f3ad382eef464fe8340f0264d23fcb
Juan José Ruiz Cruz <rcjuanjo3156@gmail.com>
2023-04-09 17:46:35 -0600

    adding sign_up.php

---
+++ src/views/sign_up.view.php
@@ -0,0 +1,19 @@
+<?php include('header_no_login.php'); ?>
+<div class="container">
+    <h2>Sign up</h2>
+    <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']);?>" method="post">
+        <label for="username">Username:</label>
+        <input type="text" id="username" name="username" class="login__input" placeholder="Enter your username">
+        <br>
+        <label for="password">Password:</label>
+        <input type="password" id="password"name="password" class="login__input" placeholder="Enter your password">
+        <input type="password" id="password"name="password2" class="login__input" placeholder="Repeat your password">
+        <br>
+        <label for="email">Email:</label>
+        <input type="email" name="email" class="login__input" placeholder="Enter your email">
+        <input type="submit" value="Submit">
+    </form>
+
+</div>
+
+<?php include('footer.php'); ?>
